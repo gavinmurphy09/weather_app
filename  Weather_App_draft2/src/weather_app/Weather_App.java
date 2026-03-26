8571245914ee680794f73ef6f54b0a024b1f71eb
gavinmurphy09 <116267423+gavinmurphy09@users.noreply.github.com>
2026-03-26 23:05:16 +0000

    Add files via upload
    Updated gui working with validation

---
+++  Weather_App_draft2/src/weather_app/Weather_App.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package weather_app;
+import GUI.*;
+/**
+ *
+ * @author pi
+ */
+public class Weather_App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI_main gui= new GUI_main();
+        gui.setVisible(true);
+    }
+    
+}
